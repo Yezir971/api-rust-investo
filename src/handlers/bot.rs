b0ac9439e5bcd2fb496e60ce9aea6263969fbe49
Yezir971 <james_ahmedaly@yahoo.com>
2026-05-06 15:05:22 +0200

    feat : add route stop

--- src/handlers/bot.rs
+++ src/handlers/bot.rs
@@ -47,44 +47,75 @@ pub async fn start_bot(
     State(state): State<AppState>,
     Json(body): Json<StartBotSchema>,
 ) -> Result<Json<serde_json::Value>, (StatusCode, String)>{
-        // Vérifie si le bot ne tourne pas déjà
-        if state.active_bots.contains_key(&body.user_id) {
-            return Err((StatusCode::BAD_REQUEST, Json(serde_json::json!({"message": "Bot déjà lancé", "status": "error"})).to_string()));
-        }
+    // Vérifie si le bot ne tourne pas déjà
+    if state.active_bots.contains_key(&body.user_id) {
+        return Err((StatusCode::BAD_REQUEST, Json(serde_json::json!({"message": "Bot déjà lancé", "status": "error"})).to_string()));
+    }
 
-        // Récupère les clés en BDD (déchiffrées)
-        let keys = api_key_repo::get_decrypted_keys(
-            &state.pool, 
-            body.user_id, 
-            state.master_encryption_key.as_bytes()
-        ).await.map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Erreur BDD".to_string()))?;
+    // Récupère les clés en BDD (déchiffrées)
+    let keys = api_key_repo::get_decrypted_keys(
+        &state.pool, 
+        body.user_id, 
+        state.master_encryption_key.as_bytes()
+    ).await.map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Erreur BDD".to_string()))?;
 
-        let key_info = keys.first().ok_or((StatusCode::NOT_FOUND, "Pas de clés".to_string()))?;
-        let url_brocker = "https://api.crypto.com/v2/";
+    let key_info = keys.first().ok_or((StatusCode::NOT_FOUND, "Pas de clés".to_string()))?;
+    let url_brocker = "https://api.crypto.com/v2/";
+    
+    
+    let exchange = Arc::new(CryptoComExchange::new(
+        key_info.key.clone(),
+        key_info.secret.clone(),
+        url_brocker.to_string()
+    ));
+
+    // 4. Spawn
+    let id_str = body.user_id;
+    let handle = tokio::spawn(async move {
+        run_brain_bot(&id_str.to_string(), exchange, "BTC_USDT".to_string()).await;
+    });
+
+    // Enregistre dans la DashMap
+    state.active_bots.insert(body.user_id, BotControl { handle });
+    
+
+    Ok(Json(serde_json::json!({
+        "status": "success",
+        "message": "Le bot est lancé" ,
+        "is_active": true,
+    
+    })))
+}
+
+pub async fn stop_bot(
+    State(state): State<AppState>,
+    Json(body): Json<StartBotSchema>,
+) -> Result<Json<serde_json::Value>, (StatusCode, String)>{
+
+    // On vérifie si le bot existait bien dans la map
+    if let Some((_, bot_control)) = state.active_bots.remove(&body.user_id) {
         
+        // 2. On tue le thread Tokio immédiatement
+        bot_control.handle.abort();
         
-        let exchange = Arc::new(CryptoComExchange::new(
-            key_info.key.clone(),
-            key_info.secret.clone(),
-            url_brocker.to_string()
-        ));
-
-        // 4. Spawn
-        let id_str = body.user_id;
-        let handle = tokio::spawn(async move {
-            run_brain_bot(&id_str.to_string(), exchange, "BTC_USDT".to_string()).await;
-        });
-
-        // Enregistre dans la DashMap
-        state.active_bots.insert(body.user_id, BotControl { handle });
-
+        println!("✅ Bot stoppé proprement pour {}", body.user_id);
 
         Ok(Json(serde_json::json!({
             "status": "success",
-            "message": "Le bot est au repos" ,
-            "is_active": "is_active",
-        
+            "message": "Le bot a été arrêté",
+            "is_active": false
         })))
-    
+    } else {
+        // 3. Si l'ID n'était pas dans la map, le bot ne tournait pas
+        println!("Aucun bot actif trouvé pour {}", body.user_id);
+        
+        let error_body = serde_json::json!({ 
+            "status": "error",
+            "message": "Le bot n'est pas en cours d'exécution",
+            "is_active": false 
+        }).to_string();
+
+        Err((StatusCode::NOT_FOUND, error_body))
+    }
 
 }
--- src/routes/bot.rs
+++ src/routes/bot.rs
@@ -1,14 +1,14 @@
 use axum::{routing::{post, get}, Router};
 use crate::schema::{AppState};
-use crate::handlers::bot::{add_api_key, status_bot, start_bot};
+use crate::handlers::bot::{add_api_key, status_bot, start_bot, stop_bot};
 
 pub fn bot_routes() -> Router<AppState> {
     Router::new()
         .route("/keys", post(add_api_key))
         .route("/status/{id}", get(status_bot))
         .route("/start", post(start_bot))
+        .route("/stop", post(stop_bot))
         // ---------- Must have --------------
-        // .route("/stop", post(add_api_key))
         // .route("/solde/:user_id", get(add_api_key))
         // ---------- Could have ------------ 
         // .route("/buy/:user_id", post(add_api_key))
